b11aeb0f0443319bf566f7f6aff01d70e03c7e78
Valentin Gartiser <valentin.gartiser@student.uni-tuebingen.de>
2022-03-04 17:21:10 +0100

    added sorting to seepr_dat objects

--- R/seepr_dat.R
+++ R/seepr_dat.R
@@ -10,7 +10,7 @@
 #'  \item{T_...}{Further temperature measurements}
 #'}
 #' @param depths A vector with the same length as the number of temperature probes in
-#' x. The depths correspond to the depth of the temperature probes in x read from left
+#' x. Depth in m from sediement/water interface (down is positive). The depths correspond to the depth of the temperature probes in x read from left
 #' to right.
 #'
 #'
@@ -29,13 +29,15 @@ seepr_dat <- function(x,
 
   stopifnot("For each temperature column a depth must be supplied!" =
               length(x) == length(depths) + 1)
+  stopifnot("NAs in depths!" = !anyNA(depths))
+  stopifnot("Duplicate depths!" = (length(depths)) == length(unique(depths)))
 
   stopifnot("Duplicates in datetime column!" =
               length(unique(x[,1])) == length(x[,1]))
   stopifnot("datetime cannot contain NAs" =
               !anyNA(x[,1]))
 
-  if(is.unsorted(x[,1])){
+  if (is.unsorted(x[,1])){
     message("datetime not in increasing order! Trying to sort things out.")
     x <- x %>%
       dplyr::arrange(datetime)
@@ -43,6 +45,13 @@ seepr_dat <- function(x,
 
   stopifnot("Data must be a regular time series and cannot contain NAs!" =
               length(unique(diff(x$datetime))) == 1)
+
+  if (is.unsorted(depths)){
+    message("rearranging data from top to bottom (left to right)")
+    dsi <- sort(depths, index.return = TRUE)
+    x <- x[,c(1, dsi+1)]
+    depths <- depths[dsi]
+  }
 
   x <- new_seepr_dat(x,
                      depths)
